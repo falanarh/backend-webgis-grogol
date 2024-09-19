89198f03a9b43cf9146939503c13d6e00a11224a
222112038 <falanarofako29@gmail.com>
2024-09-19 15:08:48 +0700

    update cors

--- src/app.ts
+++ src/app.ts
@@ -22,17 +22,24 @@ app.use(morgan("dev"));
 //   })
 // );
 
-app.use(cors({
-  origin: ['https://desa-cantik-sda.vercel.app', 'http://localhost:5173'], // Ganti dengan domain Anda
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Metode yang diizinkan
-  // allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(
+  cors({
+    origin: [
+      "https://desa-cantik-sda.vercel.app",
+      "http://localhost:5173",
+      "http://127.0.0.1:5173",
+      "https://desacantik.pahlawan140.com",
+    ], // Ganti dengan domain Anda
+    methods: ["GET", "POST", "PUT", "DELETE"], // Metode yang diizinkan
+    // allowedHeaders: ['Content-Type', 'Authorization']
+  })
+);
 
-app.use(express.json({ limit: '10mb' }));
+app.use(express.json({ limit: "10mb" }));
 // app.use(bodyParser.json());
 
 app.use("/api/auth", authRoutes);
-app.use("/api/sls", slsRoutes)
+app.use("/api/sls", slsRoutes);
 app.use("/api/usahaSayuran", usahaSayuranRoutes);
 
 export default app;
